326d0a62079c0d21870dba4655090b67e0787585
ArifurRahman7 <mda216000@gmail.com>
2025-12-25 13:51:15 -0700

    Completed DB setup, Models, Middleware, and Controllers

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { protectRoute,requireAdmin} from "../middleware/auth.middleware.js"
+import { getStats } from "../controller/stat.controller.js";
+
 
 const router = Router();
+//no auth needed for now, getting error
+//will use token later
 
-router.get('/',(req,res) => {
-    res.send("Stat route with GET method");
-});
+router.get("/",protectRoute, requireAdmin,getStats)
+// router.get("/",getStats)
 
 export default router;
