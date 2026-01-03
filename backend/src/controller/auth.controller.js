319f8272d5aa421fa31937b4bf46211c9f9cdd49
ArifurRahman7 <mda216000@gmail.com>
2026-01-02 20:47:25 -0700

    all_controller_added

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,20 +1,29 @@
-import { User } from "../models/user.model.js"
-export const authCallback = async (req, res,next) => {
-    try {
-        const { id, firstName, lastName, imageUrl } = req.body;
-          //check if user already exists
-        const user = await User.findOne({ clerkId: id });
-        if (!user) {
-            //signup
+import { User } from "../models/user.model.js";
+
+export const authCallback = async (req,res,next) => {
+    try{
+        const {id,firstName,lastName} = req.body;
+
+        //check if user already exists
+        const user = await User.findOne({clerkId:id});
+        if(!user){
+            //create new user
             await User.create({
-                clerkId: id,
-                firstName,
-                lastName,
-                imageUrl,
+                clerkId:id,
+                fullName: `${firstName || ""} ${lastName || ""}`.trim(),
+                imageUrl: req.body.imageUrl
             });
         }
-    } catch (error) {
-        console.log("Error in auth callback:", error);
-        next(error);
+        res.status(200).json({
+            success:true,
+            // message:"user created successfully",
+        })
+    }catch(err){
+        console.log("error in auth route",err);
+        // res.status(500).json({
+        //     success:false,
+        //     message:"internal server error",err
+        //})
+        next(err);
     }
-};
+}
